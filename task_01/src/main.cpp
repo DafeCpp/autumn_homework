e96ecd0783495f8485643050c49508edc9666c27
d1maal <dimaal99@mail.ru>
2025-12-27 11:34:50 +0000

    solved task_01 and task_02

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,35 +1,21 @@
+#include "graph.h"
 #include <iostream>
 
-#include "find_bridges.hpp"
-#include "find_critical_points.hpp"
-
+// В main.cpp:
 int main() {
   int n, m;
   std::cin >> n >> m;
-
-  Graph<int> graph(false);
-  for (int i = 0; i < n; i++) graph.AddVertex(i + 1);
-
+  
+  Graph g(n);
+  
   for (int i = 0; i < m; i++) {
     int u, v;
     std::cin >> u >> v;
-    graph.AddEdge(u, v);
+    //1-based в 0-based
+    u--; v--;
+    g.AddEdge(u, v);
   }
-
-  std::vector<Edge<int>> bridges = FindBridges(graph);
-  std::vector<int> points = FindJointVertices(graph);
-
-  std::cout << points.size() << std::endl;
-  if (points.size() == 0) std::cout << "-";
-  for (auto i : points) {
-    std::cout << i << " ";
-  }
-  std::cout << "\n";
-  std::cout << bridges.size() << std::endl;
-  if (bridges.size() == 0) std::cout << "-" << std::endl;
-  for (auto i : bridges) {
-    std::cout << i.v1 << " " << i.v2 << "; ";
-  }
-
+  
+  g.PrintResults();
   return 0;
 }
--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <vector>
 
-#include "districts.hpp"
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios_base;
+using std::pair;
+using std::vector;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int n, m;
-  std::cin >> n >> m;
-
-  Graph<int> graph;
-  for (int i = 0; i < n; i++) graph.AddVertex(i + 1);
-
-  for (int i = 0; i < m; i++) {
-    int u, v;
-    std::cin >> u >> v;
-    graph.AddEdge(u, v);
+  cin >> n >> m;
+  vector<pair<int, int>> edges(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> edges[i].first >> edges[i].second;
   }
 
-  std::cout << MinEdgesToMakeOneDistrict(graph);
+  cout << MinEdgesToStronglyConnected(n, edges) << endl;
+
+  return 0;
 }
