443be7c4b0c7b018195765251a811247637efba0
VladimirMukhin21 <102899387+VladimirMukhin21@users.noreply.github.com>
2026-09-19 09:03:59 +0000

    Linker issues fixed

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,70 +1,68 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
+void static create(int m, vector<vector<int>> &vect) {
+  for (int i = 0; i < m; i++) {
+    int first, second;
+    cin >> first >> second;
 
-void create(int m, vector<vector<int>> &vect) {
-    for (int i = 0; i < m; i++) {
-        int first, second;
-        cin >> first >> second;
-
-        if (first == second) {
-            cout << -1;               // найдена циклическая зависимость
-            return;
-        }
-        
-        vect[first].push_back(second);
-    }
-}
-
-void dfs(int u, vector<vector<int>> &vect, vector<char> &color, vector<int> &sorted, bool &has_cycle) {
-    if (color[u] == 'b') return;
-    color[u] = 'g';
-
-    for (int v : vect[u]) {
-        if (color[v] == 'b') {
-            has_cycle = true;
-            return;
-        }
-        if (color[u] == 'w') {
-            dfs(v, vect, color, sorted, has_cycle);
-            if (has_cycle) return;
-        }
+    if (first == second) {
+      cout << -1;  // найдена циклическая зависимость
+      return;
     }
 
-    color[u] = 'b';
-    sorted.push_back(u);
+    vect[first].push_back(second);
+  }
 }
 
+void static dfs(int u, vector<vector<int>> &vect, vector<char> &color,
+                vector<int> &sorted, bool &has_cycle) {
+  if (color[u] == 'b') return;
+  color[u] = 'g';
+
+  for (int v : vect[u]) {
+    if (color[v] == 'b') {
+      has_cycle = true;
+      return;
+    }
+    if (color[u] == 'w') {
+      dfs(v, vect, color, sorted, has_cycle);
+      if (has_cycle) return;
+    }
+  }
+
+  color[u] = 'b';
+  sorted.push_back(u);
+}
 
 int main() {
-    unsigned int n, m;
-    cin >> n >> m;
+  unsigned int n, m;
+  cin >> n >> m;
 
-    vector<vector<int>> vect(n, vector<int>());
+  vector<vector<int>> vect(n, vector<int>());
 
-    create(m, vect);
+  create(m, vect);
 
-    vector<char> color(n, 'w');
-    vector<int> sorted;
-    bool has_cycle = false;
+  vector<char> color(n, 'w');
+  vector<int> sorted;
+  bool has_cycle = false;
 
-    for (int i = 0; i < n; i++) {
-        if (color[i] == 'w') {
-            dfs(i, vect, color, sorted, has_cycle);
-            if (has_cycle) {
-                cout << -1 << endl;
-                return 0;
-            }
-        }
+  for (int i = 0; i < n; i++) {
+    if (color[i] == 'w') {
+      dfs(i, vect, color, sorted, has_cycle);
+      if (has_cycle) {
+        cout << -1 << '\n';
+        return 0;
+      }
     }
+  }
 
-    reverse(sorted.begin(), sorted.end());
+  reverse(sorted.begin(), sorted.end());
 
-    cout << endl;
-    for (int elem : sorted)
-        cout << elem << " ";
+  cout << '\n';
+  for (int elem : sorted) cout << elem << " ";
 
-    return 0;
+  return 0;
 }
