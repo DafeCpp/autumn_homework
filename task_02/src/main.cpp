b51f8b896d0011edbc5017d732f37d0c45750763
St0rm-Petrel <aleksakhin.viu@phystech.edu>
2026-09-26 08:53:17 +0000

    Formatted kosaraju.h, main.cpp and test.cpp for task_02

--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
+
 #include "kosaraju.h"
 
 int main() {
-    int n, m;
-    std::cin >> n >> m;
+  int n, m;
+  std::cin >> n >> m;
 
-    std::vector<Edge> edges(m);
-    for (int i = 0; i < m; ++i) {
-        std::cin >> edges[i].from >> edges[i].to;
-    }
+  std::vector<Edge> edges(m);
+  for (int i = 0; i < m; ++i) {
+    std::cin >> edges[i].from >> edges[i].to;
+  }
 
-    KosarajuSolver solver;
-    solver.init(n, edges);
-    std::cout << solver.solve() << "\n";
+  KosarajuSolver solver;
+  solver.init(n, edges);
+  std::cout << solver.solve() << "\n";
 
-    return 0;
+  return 0;
 }
